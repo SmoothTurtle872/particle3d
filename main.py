9ca793187162ac5519190343c25ab07cc52e95ec
SmoothTurtle872 <whatleyfamily1@gmail.com>
2024-08-19 17:40:05 +0930

    1.20.6 / 1.21 port
    Added:
      1.20.6 / 1.21 support
      File searching
      Orientation selection
    Removed:
      Old particle system
      Name input for files

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
+# - Imports
+# Pre-made
 import inquirer
+import os
+import time
+# Custom
 from colorConvert import hex_to_rgb
 
-
+# - Main Function
 def main():
+    # - User Feedback
+    print("Searching for .obj files...")
+    # - Searching for .obj files for selection
+    foundFiles = []
+    for (root, _, files) in os.walk(".", topdown=True):
+        for file in files:
+            # Checking if its a .obj file
+            if file.split(".")[-1] == "obj":
+                foundFiles.append(f"{root}{os.sep}{file}")
+    
+    # Checking if any files were found
+    if len(foundFiles) > 0:
+        print("Files Found!")
+    else:
+        print("Could Not Find Files :(")
+        time.sleep(0.5)
+        print("Quitting...")
+        time.sleep(0.5)
+        quit()
+        
+    # - Getting user Selected File
+    name = inquirer.list_input("Select a file",choices = foundFiles)
+    # - Get scaling value
     while True:
-        try:
-            name = input("What is the name of your obj file (don't include the .obj)? ")
-            f = open(f"{name}.obj")
-            f.close()
-            break
-        except FileNotFoundError:
-            print("That file does not exist!")
-    while True:
+        # Ensuring no breakages
         try:
             scaling = float(input("What scaling value would you like to use (The default blender cube is 4 blocks at scaling 1, set to 0.5 for it to be 1 block)? "))
             break
         except ValueError:
             print("Please only input numbers!")
-            
+    
+    # - Coloration     
     colorType = inquirer.list_input("What color type do you wish to use to select a color?", choices=["Hex", "RGB"])
     color = []
     
+    # - Hex support
     if colorType == "Hex":
         hexValues = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
         while True:
             try:
-                code = input("Input your hex code: ")
+                code = input("Input your hex code: ").lower()
                 if len(code) == 6:
                     for value in code:
                         if value not in hexValues:
@@ -43,17 +66,19 @@ def main():
             
     for value in code:
         color.append(value/255)
+        
+    orientation = inquirer.list_input("Choose your rotation space:", choices=["~", "^"])
             
     lines = []
-    with open(f"{name}.obj") as o:
+    with open(name) as o:
         for line in o:
             coords = line.split()
             x = float(coords[1].strip()) * scaling
             y = float(coords[2].strip()) * scaling
             z = float(coords[3].strip()) * scaling
-            lines.append(f"particle dust {color[0]} {color[1]} {color[2]} 1 ~{x} ~{y} ~{z} 0 0 0 0 1 force\n")
+            lines.append(f"particle dust{"{"}color:[{color[0]},{color[1]},{color[2]}],scale:1{"}"} {orientation}{x} {orientation}{y} {orientation}{z} 0 0 0 0 1 force\n")
     
-    with open(f"{name}.mcfunction", "w") as o:
+    with open(f"{name[0:-4]}.mcfunction", "w") as o:
         for command in lines:
             o.write(command)
 

7d93c9e0853446f6a5cce9004922bebc99fe608b
SmoothTurtle872 <whatleyfamily1@gmail.com>
2026-04-20 21:30:12 +0930

    Separated code out for an easier time
    Also started GUI development

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,30 @@
-fn main() {}
+use eframe::egui;
+
+use particle3d::cloud::ParticleCloud;
+
+static APP_NAME: &str = "Particle 3D";
+static VERSION: &str = "2.0.0";
+
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    _ = eframe::run_native(
+        &format!("{} | {}", APP_NAME, VERSION),
+        native_options,
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
+    )
+}
+
+#[derive(Default)]
+struct App {
+    cloud: Option<ParticleCloud>,
+}
+
+impl App {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for App {
+    fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {}
+}

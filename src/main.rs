fd615a783e76b7588ff655cee6f4f4a6c768f185
SmoothTurtle872 <whatleyfamily1@gmail.com>
2026-05-08 21:44:50 +0930

    Attempted to add an icon, didn't work

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use iced::window::{Settings, icon};
 use particle3d::ui::app::App;
 
 static APP_NAME: &str = "Particle 3D";
 static VERSION: &str = "2.0.0 (beta 1a)";
 
 fn main() -> iced::Result {
+    let settings = Settings {
+        icon: Some(icon::from_file_data(include_bytes!("./resources/icon.ico"), None).unwrap()),
+        ..Default::default()
+    };
     iced::application(new, App::update, App::view)
         .title(App::get_title)
         .theme(iced::Theme::Ferra)
+        .window(settings)
         .run()
 }
 
